b563e8f854005a49f913b2974a1d6f6234d35fc1
Matthew Aliberto <maliberto3@gatech.edu>
2025-12-11 09:10:11 -0500

    Added replace filter to extras and implemented it into result.html .

--- verifier/templatetags/verifier_extras.py
+++ verifier/templatetags/verifier_extras.py
@@ -8,3 +8,7 @@ def get_item(dict, key):
     if type(item) == list:
         return item[0].replace(',','')
     return item
+
+@register.filter
+def replace(item, chars):
+    return item.replace(chars[0], chars[1])
